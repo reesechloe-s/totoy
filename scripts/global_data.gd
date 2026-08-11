1009a658b9132a057c3dab82fa167fa39c7c0379
Reese Chloe Santiago <226453167+reesechloe-s@users.noreply.github.com>
2026-08-11 14:04:04 +0800

    added signals, tracking logic for coins, time, unlocked leveels

--- scripts/global_data.gd
+++ scripts/global_data.gd
@@ -1,14 +1,48 @@
 extends Node
 
+signal barya_changed(new_amount)
+signal time_changed(new_time)
+
+var unlocked_levels: Dictionary = {}
 var barya_coins: int = 0
 var piso_timer: float = 300.0 # 5 minutes (in seconds)[cite: 1]
 var unlocked_cards: Array = []
 
 # Called when the node enters the scene tree for the first time.
+# check id and unlocked status
 func _ready() -> void:
-	pass # Replace with function body.
+	var all_data = JsonLoader.get_all_scenarios()
+	for level_data in all_data["levels"]:
+		var level_id = level_data["id"]
+		var is_unlocked = level_data["unlocked"]
+		unlocked_levels[level_id] = is_unlocked
 
+# we will only add barya, not subtract it. subtracting is only applicable to time
+func add_barya(amount: int) -> void:
+	barya_coins += amount
+	barya_changed.emit(barya_coins)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	pass
+# spending at gacha
+func spend_barya(amount: int) -> bool:
+	if barya_coins >= amount:
+		barya_coins -= amount
+		barya_changed.emit(barya_coins)
+		return true # successful purchase
+	return false
+
+func update_time(amount: float) -> void:
+	piso_timer += amount
+	if piso_timer < 0.0:
+		piso_timer = 0.0
+	time_changed.emit(piso_timer)
+
+# level progression
+## to be called in level selection menu
+func is_level_unlocked(level_id: int) -> bool:
+	return unlocked_levels[level_id]
+
+## when totoy beats a level, the next level shall be unlocked
+func unlock_level(level_id: int) -> void:
+	unlocked_levels[level_id] = true
+
+# gacha_cards
